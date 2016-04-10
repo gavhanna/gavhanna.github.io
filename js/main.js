16d470f30bc46d1dee4ef6be349e5dee0df0068d
gavhanna <gavhanna@gmail.com>
2016-04-10 15:21:05 +0100

    added pikabu sidebars

--- js/main.js
+++
@@ -1,9 +0,0 @@
-$('.sites').click(function() {
-        $('.alan').slideToggle("fast");
-        $('.gary').fadeToggle("fast");
-});
-
-$('.apps').click(function() {
-        $('.weather').slideToggle("fast");
-        $('.wiki-viewer').fadeToggle("fast");
-});
