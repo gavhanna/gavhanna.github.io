9a79c887a66a24ffa192cc5e30039ba30d20c2ae
Gavin Hanna <gavhanna@gmail.com>
2016-10-09 16:50:14 +0100

    all side panels and links added/working

--- js/main.js
+++ js/main.js
@@ -4,26 +4,38 @@
       $(".left-button").toggleClass("hide");
       $(".right-button").toggleClass("hide");
       $(".left-sidebar").toggleClass("open closed");
-      console.log($btn);
+      $(".clickable-area").addClass("high-z-index");
     });
 
     $(".right-button").on("click", function(){
       $(".left-button").toggleClass("hide");
       $(".right-button").toggleClass("hide");
       $(".right-sidebar").toggleClass("open closed");
-      console.log($btn);
+      $(".clickable-area").addClass("high-z-index");
     });
 
     $(".close-left-sidebar").on("click", function(){
       $(".left-button").toggleClass("hide");
       $(".right-button").toggleClass("hide");
       $(".left-sidebar").toggleClass("open closed");
+      $(".clickable-area").removeClass("high-z-index");
     });
 
     $(".close-right-sidebar").on("click", function(){
       $(".left-button").toggleClass("hide");
       $(".right-button").toggleClass("hide");
       $(".right-sidebar").toggleClass("open closed");
+      $(".clickable-area").removeClass("high-z-index");
+    });
+
+    $(".clickable-area").on("click", function(){
+      $(this).removeClass("high-z-index");
+      $(".left-button").toggleClass("hide");
+      $(".right-button").toggleClass("hide");
+      $(".left-sidebar").removeClass("open");
+      $(".left-sidebar").addClass("closed");
+      $(".right-sidebar").removeClass("open");
+      $(".right-sidebar").addClass("closed");
     });
 
 })();
