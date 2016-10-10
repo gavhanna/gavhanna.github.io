b6ec2600321aba96e770eb66daff6b9f36856f12
Gavin Hanna <gavhanna@gmail.com>
2016-10-10 23:04:24 +0100

    added about page sidebar

--- js/main.js
+++ js/main.js
@@ -1,8 +1,8 @@
-(function(){
-
+jQuery(function ($) {
     $(".left-button").on("click", function(){
       $(".left-button").toggleClass("hide");
       $(".right-button").toggleClass("hide");
+      $(".middle-button").toggleClass("hide");
       $(".left-sidebar").toggleClass("open closed");
       $(".clickable-area").addClass("high-z-index");
     });
@@ -10,13 +10,23 @@
     $(".right-button").on("click", function(){
       $(".left-button").toggleClass("hide");
       $(".right-button").toggleClass("hide");
+      $(".middle-button").toggleClass("hide");
       $(".right-sidebar").toggleClass("open closed");
+      $(".clickable-area").addClass("high-z-index");
+    });
+
+    $(".middle-button").on("click", function(){
+      $(".left-button").toggleClass("hide");
+      $(".right-button").toggleClass("hide");
+      $(".middle-button").toggleClass("hide");
+      $(".middle-sidebar").toggleClass("open closed");
       $(".clickable-area").addClass("high-z-index");
     });
 
     $(".close-left-sidebar").on("click", function(){
       $(".left-button").toggleClass("hide");
       $(".right-button").toggleClass("hide");
+      $(".middle-button").toggleClass("hide");
       $(".left-sidebar").toggleClass("open closed");
       $(".clickable-area").removeClass("high-z-index");
     });
@@ -24,18 +34,31 @@
     $(".close-right-sidebar").on("click", function(){
       $(".left-button").toggleClass("hide");
       $(".right-button").toggleClass("hide");
+      $(".middle-button").toggleClass("hide");
       $(".right-sidebar").toggleClass("open closed");
       $(".clickable-area").removeClass("high-z-index");
     });
+
+    $(".close-middle-sidebar").on("click", function(){
+      $(".left-button").toggleClass("hide");
+      $(".right-button").toggleClass("hide");
+      $(".middle-button").toggleClass("hide");
+      $(".middle-sidebar").toggleClass("open closed");
+      $(".clickable-area").removeClass("high-z-index");
+    });
+
 
     $(".clickable-area").on("click", function(){
       $(this).removeClass("high-z-index");
       $(".left-button").toggleClass("hide");
       $(".right-button").toggleClass("hide");
+      $(".middle-button").toggleClass("hide");
       $(".left-sidebar").removeClass("open");
       $(".left-sidebar").addClass("closed");
       $(".right-sidebar").removeClass("open");
       $(".right-sidebar").addClass("closed");
+      $(".middle-sidebar").removeClass("open");
+      $(".middle-sidebar").addClass("closed");
     });
 
-})();
+});
