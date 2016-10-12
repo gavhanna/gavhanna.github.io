67963415cf108b67c5d5b28ce107368fbd7f967d
Gavin Hanna <gavhanna@gmail.com>
2016-10-12 16:16:45 +0100

    made sidebars responsive

--- js/main.js
+++ js/main.js
@@ -1,64 +1,66 @@
 jQuery(function ($) {
-    $(".left-button").on("click", function(){
-      $(".left-button").toggleClass("hide");
-      $(".right-button").toggleClass("hide");
-      $(".middle-button").toggleClass("hide");
-      $(".left-sidebar").toggleClass("open closed");
-      $(".clickable-area").addClass("high-z-index");
-    });
-
-    $(".right-button").on("click", function(){
-      $(".left-button").toggleClass("hide");
-      $(".right-button").toggleClass("hide");
-      $(".middle-button").toggleClass("hide");
-      $(".right-sidebar").toggleClass("open closed");
-      $(".clickable-area").addClass("high-z-index");
-    });
-
-    $(".middle-button").on("click", function(){
-      $(".left-button").toggleClass("hide");
-      $(".right-button").toggleClass("hide");
-      $(".middle-button").toggleClass("hide");
-      $(".middle-sidebar").toggleClass("open closed");
-      $(".clickable-area").addClass("high-z-index");
-    });
-
-    $(".close-left-sidebar").on("click", function(){
-      $(".left-button").toggleClass("hide");
-      $(".right-button").toggleClass("hide");
-      $(".middle-button").toggleClass("hide");
-      $(".left-sidebar").toggleClass("open closed");
-      $(".clickable-area").removeClass("high-z-index");
-    });
-
-    $(".close-right-sidebar").on("click", function(){
-      $(".left-button").toggleClass("hide");
-      $(".right-button").toggleClass("hide");
-      $(".middle-button").toggleClass("hide");
-      $(".right-sidebar").toggleClass("open closed");
-      $(".clickable-area").removeClass("high-z-index");
-    });
-
-    $(".close-middle-sidebar").on("click", function(){
-      $(".left-button").toggleClass("hide");
-      $(".right-button").toggleClass("hide");
-      $(".middle-button").toggleClass("hide");
-      $(".middle-sidebar").toggleClass("open closed");
-      $(".clickable-area").removeClass("high-z-index");
-    });
 
 
-    $(".clickable-area").on("click", function(){
-      $(this).removeClass("high-z-index");
-      $(".left-button").toggleClass("hide");
-      $(".right-button").toggleClass("hide");
-      $(".middle-button").toggleClass("hide");
-      $(".left-sidebar").removeClass("open");
-      $(".left-sidebar").addClass("closed");
-      $(".right-sidebar").removeClass("open");
-      $(".right-sidebar").addClass("closed");
-      $(".middle-sidebar").removeClass("open");
-      $(".middle-sidebar").addClass("closed");
-    });
+  $(".left-button").on("click", function(){
+    $(".left-button").toggleClass("hide");
+    $(".right-button").toggleClass("hide");
+    $(".middle-button").toggleClass("hide");
+    $(".left-sidebar").toggleClass("open closed");
+    $(".clickable-area").addClass("high-z-index");
+  });
+
+  $(".right-button").on("click", function(){
+    $(".left-button").toggleClass("hide");
+    $(".right-button").toggleClass("hide");
+    $(".middle-button").toggleClass("hide");
+    $(".right-sidebar").toggleClass("open closed");
+    $(".clickable-area").addClass("high-z-index");
+  });
+
+  $(".middle-button").on("click", function(){
+    $(".left-button").toggleClass("hide");
+    $(".right-button").toggleClass("hide");
+    $(".middle-button").toggleClass("hide");
+    $(".middle-sidebar").toggleClass("open closed");
+    $(".clickable-area").addClass("high-z-index");
+  });
+
+  $(".close-left-sidebar").on("click", function(){
+    $(".left-button").toggleClass("hide");
+    $(".right-button").toggleClass("hide");
+    $(".middle-button").toggleClass("hide");
+    $(".left-sidebar").toggleClass("open closed");
+    $(".clickable-area").removeClass("high-z-index");
+  });
+
+  $(".close-right-sidebar").on("click", function(){
+    $(".left-button").toggleClass("hide");
+    $(".right-button").toggleClass("hide");
+    $(".middle-button").toggleClass("hide");
+    $(".right-sidebar").toggleClass("open closed");
+    $(".clickable-area").removeClass("high-z-index");
+  });
+
+  $(".close-middle-sidebar").on("click", function(){
+    $(".left-button").toggleClass("hide");
+    $(".right-button").toggleClass("hide");
+    $(".middle-button").toggleClass("hide");
+    $(".middle-sidebar").toggleClass("open closed");
+    $(".clickable-area").removeClass("high-z-index");
+  });
+
+
+  $(".clickable-area").on("click", function(){
+    $(this).removeClass("high-z-index");
+    $(".left-button").toggleClass("hide");
+    $(".right-button").toggleClass("hide");
+    $(".middle-button").toggleClass("hide");
+    $(".left-sidebar").removeClass("open");
+    $(".left-sidebar").addClass("closed");
+    $(".right-sidebar").removeClass("open");
+    $(".right-sidebar").addClass("closed");
+    $(".middle-sidebar").removeClass("open");
+    $(".middle-sidebar").addClass("closed");
+  });
 
 });
