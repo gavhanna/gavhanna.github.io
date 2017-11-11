90832a34fe9eaa95ff3be591cea8ca336b1d2e9d
Gav Hanna <gavhanna@gmail.com>
2017-11-11 12:09:58 +0000

    seems to be working

---
+++ sw.js
@@ -0,0 +1,50 @@
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', function() {
+    navigator.serviceWorker.register('sw.js').then(function(registration) {
+      console.log('ServiceWorker registration successful with scope: ', registration.scope);
+    }, function(err) {
+      console.log('ServiceWorker registration failed: ', err);
+    });
+  });
+}
+
+let staticCacheName = 'static-files-v2';
+
+self.addEventListener('install', function(event) {
+  
+  const urlsToCache = [
+    '/',
+    'css/main.css',
+    'js/main.js',
+    'https://fonts.googleapis.com/css?family=Raleway|Satisfy'
+  ];
+  
+  event.waitUntil(
+    caches.open(staticCacheName).then(function(cache) {
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+self.addEventListener('activate', function(event) {
+  event.waitUntil(
+    caches.keys().then(function(cacheNames) {
+      return Promise.all(
+        cacheNames.filter(function(cacheName) {
+          return cacheName.startsWith('static-files-') && 
+                 cacheName != staticCacheName
+        }).map(function(cacheName) {
+          return caches.delete(cacheName);
+        })
+      )
+    })
+  );
+});
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request).then(function(response){
+      return response || fetch(event.request);
+    })
+  );
+});
